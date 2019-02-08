b81c4d96250db7790ecd9fb5ff3d405c6c7c9e9b
jacob johnston <jacob@jacobjangles.com>
2019-02-08 21:48:49 +1100

    Turns out firewall-cmd commands don't persist unless you use --permanent

--- teamspeak-setup-configure-firewalld.sh
+++ teamspeak-setup-configure-firewalld.sh
@@ -12,18 +12,18 @@ echo "Do you use TSDNS? (If you're not sure, say no)"
 select yn in "Yes" "No"; do
   case $yn in
     Yes ) printf "Allowing 41144/tcp...\n";
-      firewall-cmd --zone=public --add-port=41144/tcp;
+      firewall-cmd --zone=public --add-port=41144/tcp --permanent;
       break;;
     No ) echo "Skipping TSDNS"
       break;;
     esac
 done
 printf "Allowing 9987/udp...\n"
-firewall-cmd --zone=public --add-port=9987/udp
+firewall-cmd --zone=public --add-port=9987/udp --permanent
 printf "Allowing 30033/tcp...\n"
-firewall-cmd --zone=public --add-port=30033/tcp
+firewall-cmd --zone=public --add-port=30033/tcp --permanent
 printf "Allowing 10011/tcp...\n"
-firewall-cmd --zone=public --add-port=10011/tcp
+firewall-cmd --zone=public --add-port=10011/tcp --permanent
 printf "${outgreen}Done.\n${outdefault}"
 printf "${outmagenta}Here's the current FirewallD config:\n${outdefault}"
 firewall-cmd --list-all
--- teamspeak-teardown-deny-firewalld.sh
+++ teamspeak-teardown-deny-firewalld.sh
@@ -10,13 +10,13 @@ source config.sh
 # Denying FirewallD ports
 printf "-- Denying ports with FirewallD --\n"
 printf "Denying 41144/tcp (IPv4 and IPv6)...\n";
-firewall-cmd --zone=public --remove-port=41144/tcp;
+firewall-cmd --zone=public --remove-port=41144/tcp --permanent;
 printf "Denying 9987/udp (IPv4 and IPv6)...\n"
-firewall-cmd --zone=public --remove-port=9987/udp
+firewall-cmd --zone=public --remove-port=9987/udp --permanent
 printf "Denying 30033/tcp (IPv4 and IPv6)...\n"
-firewall-cmd --zone=public --remove-port=30033/tcp
+firewall-cmd --zone=public --remove-port=30033/tcp --permanent
 printf "Denying 10011/tcp (IPv4 and IPv6)...\n"
-firewall-cmd --zone=public --remove-port=10011/tcp
+firewall-cmd --zone=public --remove-port=10011/tcp --permanent
 
 printf "${outred}FirewallD is now denying on the listed ports.\n${outdefault}"
 printf "${outgreen}Done.\n${outdefault}"
